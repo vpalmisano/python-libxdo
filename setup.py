fb235f1f4c575050bd350923485398a92138fcbb
Vittorio Palmisano <vpalmisano@gmail.com>
2019-10-16 12:58:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ install_requires = ['six']
 
 here = os.path.dirname(__file__)
 
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as fp:
-    longdesc = fp.read()
+#with open(os.path.join(here, 'README.rst'), encoding='utf-8') as fp:
+#    longdesc = fp.read()
 
-with open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8') as fp:
-    longdesc += "\n\n" + fp.read()
+#with open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8') as fp:
+#    longdesc += "\n\n" + fp.read()
 
 setup(
     name='python-libxdo',
